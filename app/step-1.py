79a3481f2ec58fe0d4b839753eda8eb43d658b6c
Denperidge <denperidge@gmail.com>
2022-10-30 21:08:51 +0100

    Fixed timezone bug, first succesful ics generation

--- app/step-1.py
+++ app/step-1.py
@@ -71,7 +71,8 @@ class Event(object):
 
     def __init__(self, name, datetime, location, url=""):
         self.name = name
-        self.datetime = parser.parse(datetime).isoformat()
+        # Replacement due to bug ? https://github.com/dateutil/dateutil/issues/70#issuecomment-945080282
+        self.datetime = parser.parse(datetime.replace("UTC", "")).isoformat()
         self.location = location
         self.url = url
 
