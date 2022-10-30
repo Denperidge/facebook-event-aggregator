9a4b197d27aa42daf9cefac1e3cc56f8ad0ac486
Denperidge <denperidge@gmail.com>
2022-10-30 19:16:42 +0100

    Bugfixes & changes
    Added nosuchelement handling, fixed env parsing, improved parse log

--- app/Step-1.py
+++ app/Step-1.py
@@ -2,6 +2,7 @@
 from pprint import pprint
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -22,7 +23,7 @@ def parse_page():
     event_container = driver.find_element(By.XPATH, """//div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div/div/div/div/div/div[3]""")
     events = event_container.find_elements(By.XPATH, "*")
     for event in events:
-        print("Detected upcoming event!")
+        print("Detected upcoming event in {}!".format("page"))
         print(event.text)
         print()
 
@@ -30,21 +31,19 @@ def parse_community():
     event_container = driver.find_element(By.ID, "upcoming_events_card").find_element(By.TAG_NAME, "table")
     events = event_container.find_elements(By.TAG_NAME, "tr")
     for event in events:
-        print("Detected upcoming event!")
+        print("Detected upcoming event in {}!".format("community"))
         print(event.text)
         print()
 
 
-print(getenv("pages"))
 """ LOADING & PARSING PAGES FROM .ENV """
 raw_pages = loads(getenv("pages"))
-print(raw_pages)
 pages = []
 for raw_page in raw_pages:
-    type = raw_page[0].lower().strip()
+    page_type = raw_page[0].lower().strip()
     url = raw_page[1]
 
-    match type:
+    match page_type:
         case "page":
             func = parse_page
         case "community":
@@ -55,14 +54,15 @@ for raw_page in raw_pages:
     pages.append((func, url))
         
 
-
 """ RUNNING PARSE FUNCTIONS ON PAGES """
 for page in pages:
-    print(page[1])
     driver.get(page[1])
     sleep(5)
     try:
         page[0]()
+
+    except NoSuchElementException:
+        print("No events found for " + page[1])
 
     except Exception as e:
         print("Error")
