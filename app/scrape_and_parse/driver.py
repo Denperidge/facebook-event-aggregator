f29f7cc34a6566fc0e9180e16a00559fc4b33abd
Denperidge <denperidge@gmail.com>
2022-11-01 17:13:15 +0100

    Removed unused import from driver.py

--- app/scrape_and_parse/driver.py
+++ app/scrape_and_parse/driver.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 
 # Setup Driver   
 # 
