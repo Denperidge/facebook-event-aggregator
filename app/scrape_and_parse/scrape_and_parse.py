8af54d605e6a743ae0b745a1f55a7a3894eb5cd1
Denperidge <denperidge@gmail.com>
2022-11-01 17:21:18 +0100

    Moved Facebook login to seperate file

--- app/scrape_and_parse/scrape_and_parse.py
+++ app/scrape_and_parse/scrape_and_parse.py
@@ -41,9 +41,3 @@ def parse_community(driver, logged_in):
         return event
 
 
-def handle_fb_login(driver, email, password):
-    driver.get("https://www.facebook.com/login")
-    driver.find_element(By.ID, "email").send_keys(email)
-    driver.find_element(By.ID, "pass").send_keys(password)
-    driver.find_element(By.TAG_NAME, "form").submit()
-    sleep(5)
--- app/step-1.py
+++ app/step-1.py
@@ -3,6 +3,7 @@ from datetime import date
 from operator import truediv
 from pprint import pprint
 from scrape_and_parse.driver import setup_driver
+from scrape_and_parse.fb_login import handle_fb_login
 from selenium.common.exceptions import NoSuchElementException
 
 from time import sleep
@@ -11,7 +12,6 @@ from json import loads, dump
 from os import getenv, makedirs
 from os.path import realpath, join, abspath, dirname
 from sys import argv
-from getpass import getpass
 from repo import init_repo_if_not_exists
 
 
@@ -35,6 +35,8 @@ if __name__ == "__main__":
     if (argv[1] == "headless"):
         headless = True
     driver = setup_driver(headless)
+
+    handle_fb_login(headless)
  
     """ LOADING & PARSING PAGES FROM .ENV """
     raw_pages = loads(getenv("pages"))
@@ -53,30 +55,6 @@ if __name__ == "__main__":
         
         pages.append((func, url))
     
-    # If an email is provided, log into Facebook
-    logged_in = False
-    if getenv("facebook_email") is not None:
-        facebook_email = getenv("facebook_email")
-        print("Facebook email passed...")
-        # Password provided, use that
-        if getenv("facebook_password") is not None:
-            print("... and a password. Using that to login.")
-            facebook_password = getenv("facebook_password")
-            handle_fb_login(facebook_email, facebook_password)
-            logged_in = True
-
-        # No password provided, but running interactively, so prompt
-        elif not headless:
-            print("... but no password. Please enter it in the following prompt:")
-            facebook_password = getpass()
-            handle_fb_login(facebook_email, facebook_password)
-            logged_in = True
-        
-        # No password provided, but running headless, so quit
-        else:
-            print("Email was passed, but no password in headless mode. Skipping login")    
-
-
 
     """ RUNNING PARSE FUNCTIONS ON PAGES """
     events = []
