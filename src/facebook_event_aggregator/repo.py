edcf8c413fbfc215a08c2b096a1d8f6885c0f6a3
Denperidge <denperidge@gmail.com>
2023-10-23 13:16:09 +0200

    Finished repo.py rework+test, pycov print missing

--- src/facebook_event_aggregator/repo.py
+++ src/facebook_event_aggregator/repo.py
@@ -7,40 +7,36 @@ from git import Repo
 
 """This module handles Git repo stuff"""
 
-def _normalize_path(dir):
+def _normalize_path(dir) -> str:
     return realpath(dir)
 
-def clone_or_pull_repo(parent_dir_path: str, dest_path: str, repo_url: str):
-    dest_path = _normalize_path(join(parent_dir_path, dest_path))
+def _get_repo_path(parent_dir_path, repo_path) -> str:
+    repo_path = _normalize_path(join(str(parent_dir_path), str(repo_path)))
+    return repo_path
 
-    if not exists(dest_path):
-        return Repo.clone_from(repo_url, dest_path)
+def _generate_commit_message():
+    current_date = datetime.now().strftime("%d %m %Y")
+    commit_msg = "Update from {}".format(current_date)
+    return commit_msg
+
+
+def clone_or_pull_repo(parent_dir_path, clone_dirname, repo_url: str):
+    repo_path = _get_repo_path(parent_dir_path, clone_dirname)
+
+    if not exists(repo_path):
+        return Repo.clone_from(repo_url, repo_path)
     else:
-        return Repo(dest_path).remote("origin").pull()
-        
-    
-def update_repo(dir, commit_msg=None):
-    dir = _normalize_path(dir)
-    if commit_msg is None:
-        current_date = datetime.now().strftime("%d %m %Y")
-        commit_msg = "Update from {}".format(current_date)
-    
-    print(commit_msg)
+        return Repo(repo_path).remote("origin").pull()
 
+    
+def update_repo(parent_dir_path, clone_dirname, commit_msg=None):
+    repo_path = _get_repo_path(parent_dir_path, clone_dirname)
+    
+    if commit_msg is None:
+        commit_msg = _generate_commit_message()
+    
+    repo = Repo(repo_path)
     
     repo.index.add(".")
-    repo.index.commit()
+    repo.index.commit(commit_msg)
     repo.remote("origin").push()
-
-
-# Thanks to https://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git#comment20583319_12791408
-# & https://stackoverflow.com/a/17938274
-def pull_update_if_needed(dir):
-    _git_command(dir, "fetch")
-    fetch = int(run("git rev-list HEAD...@{u} --count", cwd=dir, encoding="UTF-8", capture_output=True, shell=True).stdout)
-    if fetch == 0:
-        return False
-    else:
-        _git_command(dir, "pull")
-        return True
-        
--- src/tests/test_repo.py
+++ src/tests/test_repo.py
@@ -1,31 +1,57 @@
 from os.path import exists, realpath
 from pathlib import Path
+from datetime import datetime
 
-from src.facebook_event_aggregator.repo import _normalize_path, clone_or_pull_repo, update_repo, pull_update_if_needed
+from src.facebook_event_aggregator.repo import _normalize_path, _get_repo_path, _generate_commit_message, clone_or_pull_repo, update_repo
 
 from git import Repo, FetchInfo
 from git.util import IterableList
+from git.exc import GitCommandError
 
-class TestRepo():
-    def repo_dest(self, tmp_path: Path):
-        return tmp_path.joinpath("test")
-    
-    def clone_or_pull_shortcut(self, tmp_path: Path):
-        return clone_or_pull_repo(str(self.repo_dest(tmp_path)), "test", "https://github.com/github/dev.git")
-
+class TestRepo():    
+    """Test helper funcs"""
     def test_normalize_path(self):
         assert _normalize_path("../") == realpath("../")
     
-    def clone_repo_if_not_exists(self, tmp_path: Path):
-        assert not self.repo_dest(tmp_path).exists()
+    def test_get_repo_path(self, tmp_path: Path):
+        assert str(tmp_path.joinpath("test")) == _get_repo_path(tmp_path, "test")
+
+    def test_generate_commit_message(self):
+        assert _generate_commit_message() == f'Update from {datetime.now().strftime("%d %m %Y")}'
+
+
+    def clone_or_pull_shortcut(self, tmp_path: Path):
+        return clone_or_pull_repo(tmp_path, "test", "https://github.com/github/dev.git")
+
+    def test_clone_or_pull(self, tmp_path: Path):
+        repo_dir = _get_repo_path(tmp_path, "test")
+
+        assert not exists(repo_dir)
         
         assert type(self.clone_or_pull_shortcut(tmp_path)) == Repo
-        assert self.repo_dest.exists()
+        assert exists(repo_dir)
 
-        assert type(self.clone_or_pull_shortcut(tmp_path)) == IterableList[FetchInfo]
+        assert type(self.clone_or_pull_shortcut(tmp_path)) == IterableList
+        
         
     def test_update_repo(self, tmp_path: Path):
-        assert not self.repo_dest(tmp_path).exists()
+        repo_dir = _get_repo_path(tmp_path, "test")
+
+        assert not exists(repo_dir)
         
         repo = self.clone_or_pull_shortcut(tmp_path)
         
+        try:
+            generated_commit_msg = _generate_commit_message()  # Do this before so there's less chance of bad timing failing the test
+            update_repo(tmp_path, "test", commit_msg=None)
+        except GitCommandError:
+            pass  # This will return 403, which is normal
+        finally:
+            assert repo.commit(repo.branches[0]).message == generated_commit_msg
+        
+        try:
+            update_repo(tmp_path, "test", commit_msg="Test succeeded!")
+        except GitCommandError:
+            pass  # This will return 403, which is normal
+        finally:
+            assert repo.commit(repo.branches[0]).message == "Test succeeded!"
